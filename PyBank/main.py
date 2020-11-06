66baa1927338ce1118f95a68e4b231a30f96313d
John Forbis <70497094+jforbis@users.noreply.github.com>
2020-11-05 23:25:05 -0600

    saving after opening file code and notes

--- PyBank/main.py
+++ PyBank/main.py
@@ -2,3 +2,34 @@
 import os
 import csv
 
+# Specifying how to find my file.
+filepath = os.path.join("..","Resources","budget_data")
+
+# Specifying how to open and read the file.
+with open(filepath) as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=",")
+    
+    date = []
+    profit_loss = []
+
+    for row in csvreader:
+        #grab date and store it in 'date' list
+        #grab floater value from profit_loss and store in list
+
+    #do calculations here on the data 
+    #calculate number of months *try count row*
+    #calculate total for profit_loss column
+    #calculate average change (add everything up then divide by row count)
+    #Greatest increase in profits (list month and amount)
+    #greatest decrease in profits (list month and amount)
+
+
+# fileoutput = os.path.join("budget_analysis")
+# 
+#with open(fileoutput, "w", newline=" ") as datafile:
+#   writer = csv.writer(datafile)
+#   writer.writerow(...list out data column headers...)
+#   writer.writerows(...variable for your zipped list...)
+#
+
+
