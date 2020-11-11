2aa60b8fef22cc7826f1a8a87ff27a99fac4c738
John Forbis <John.Forbis@civicplus.com>
2020-11-10 22:48:58 -0600

    still cannot get the formula right for average change - saving work though.

--- PyBank/main.py
+++ PyBank/main.py
@@ -9,16 +9,20 @@ csvpath = os.path.join('Resources','budget_data.csv')
 with open(csvpath) as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
     next(csvreader)
+    listrows = list(csvreader)
+    print(listrows)
     
     date = []
     profit_loss = []
+    avg_change = []
 
     for row in csvreader:
         #grab date and store it in 'date' list
         date.append(row[0])
         #grab floater value from profit_loss and store in list
         profit_loss.append(row[1])
-
+        change = 
+        
     #print outputs
     print("Financial Analysis")
     print("----------------------------")
@@ -27,16 +31,22 @@ with open(csvpath) as csvfile:
     #this for loop is saying, 'for every row in the column of profit loss, change each value to an integer'
     for i in range(0,len(profit_loss)):
         profit_loss[i] = int(profit_loss[i])
-    #sum calculation to add profit_loss column in file (should equal 38,382,578)
-    print(sum(profit_loss))
-    #calculate average change (add everything up then divide by row count)
+        
+    #declare 'x' variable and calculate profit_loss column in file (should equal 38,382,578)
+    x = sum(profit_loss)
+    #print(f"Total: " + '${:.2f}'.format(x)) - SAVE FOR LATER WHEN DOING AVERAGE CHANGE
+    print(f"Total: $" + str(x))
+    #declare 'y' variable and calculate average change (add everything up then divide by row count)
+    #calculation to get amount changed from row to row
+    print(profit_loss(row[i])-profit_loss(row[i-1]))
+                
+    y = (sum(profit_loss)/len(profit_loss))
     #Greatest increase in profits (list month and amount)
     #greatest decrease in profits (list month and amount)
 
 
-# fileoutput = os.path.join("budget_analysis")
-# 
-#with open(fileoutput, "w", newline=" ") as datafile:
+# fileoutput = os.path.join("budget_analysis"
+# with open(fileoutput, "w", newline=" ") as datafile:
 #   writer = csv.writer(datafile)
 #   writer.writerow(...list out data column headers...)
-#   writer.writerows(...variable for your zipped list...)uuuuuu
+#   writer.writerows(...variable for your zipped list...)
