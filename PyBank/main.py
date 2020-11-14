e917f99ade6d01ff37e9af298a1ed523c7de1b8f
John Forbis <johnforbis@johns-mbp.lan>
2020-11-13 19:50:24 -0600

    Finished first part of homework 'PyBank' and uploading it.

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,4 +1,4 @@
-#importing my modules so I can reference my file path and file that I'm going to read.
+# Importing my modules so I can reference my file path and file that I'm going to read.
 import os
 import csv
 
@@ -10,54 +10,69 @@ with open(csvpath) as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
     next(csvreader)
     
-    #listrows = list(csvreader)
-    #print(listrows)
-    
+    # Creating my first two lists to hold the original csv data.
     date = []
     profit_loss = []
 
+    # For loop to go through original data and store values to lists specified above.
     for row in csvreader:
-        #grab date and store it in 'date' list
+        # Grab date and store it in 'date' list
         date.append(row[0])
-        #grab floater value from profit_loss and store in list
+        # Grab floater value from profit_loss and store in list
         profit_loss.append(row[1])
         
-    #print outputs
+    # Print outputs
     print("Financial Analysis")
     print("----------------------------")
-    #calculate number of months
+    
+    # Calculate number of months
     print("Total Months: ", len(date))
-    #this for loop is saying, 'for every row in the column of profit loss, change each value to an integer'
+    
+    # This for loop is saying, 'for every row in the column of profit loss, change each value to an integer'
     for i in range(0,len(profit_loss)):
         profit_loss[i] = int(profit_loss[i])
         
-    #declare 'x' variable and calculate profit_loss column in file (should equal 38,382,578)
+    # Declare 'x' variable and calculate profit_loss column in file (should equal 38,382,578)
     x = sum(profit_loss)
-    #print(f"Total: " + '${:.2f}'.format(x)) - SAVE FOR LATER WHEN DOING AVERAGE CHANGE
+
+    # Printing the total of the profit_loss column
     print(f"Total: $" + str(x))
-    #declare 'y' variable and calculate average change (add everything up then divide by row count)
-    #calculation to get amount changed from row to row
+
+    # Creating their list to hold values of the changes from month to month from the profit_loss column.
     average_changes = []
 
+    # For loop to go through the profit_loss column and calculate the change from month to month.
     for j in range(1,len(profit_loss)):
-        change = (profit_loss[j])-(profit_loss[j-1])
+        change = (profit_loss[j]) - (profit_loss[j-1])
         average_changes.append(change)
     
+    # Zipping my lists together.
+    zipper = zip(date,profit_loss,average_changes)
+    
+    # Variable to hold the zipped data in a list.
+    z = list(zipper)
+    
+    # Variable to calculate the average of "average_changes" column.
     avg_rate_change = (sum(average_changes))/len(average_changes)
+    
+    # Printing the average of changes for the period.
     print(f"Average Change: " + '${:.2f}'.format(avg_rate_change))
     
-    #Greatest increase in profits (list month and amount)
+    # Greatest increase in profits (list month and amount)
     max_change = max(average_changes)
-    #use 'zip' to pull in the corresponding date info
-    print(f"Greatest Increase in Profits: " + '${:.2f}'.format(max_change))
-    #print(f"Greatest Increase in Profits: {max_change})
-    #greatest decrease in profits (list month and amount)
+
+    # Printing the greatest increase in profit for the period showing the amount and month/year.
+    print(f"Greatest Increase in Profits: " + (z[(average_changes.index(max_change))+1][0]) + " " + '${:.2f}'.format(max_change))
+    
+    # Greatest decrease in profits (list month and amount)
     min_change = min(average_changes)
-    print(f"Greatest Decrease in Profits: " + '${:.2f}'.format(min_change))
+    
+    # Printing the greatest decrease in profit for the period showing the amount and month/year.
+    print(f"Greatest Decrease in Profits: " + (z[(average_changes.index(min_change))+1][0]) + " " + '${:.2f}'.format(min_change))
 
-
-# fileoutput = os.path.join("budget_analysis"
-# with open(fileoutput, "w", newline=" ") as datafile:
-#   writer = csv.writer(datafile)
-#   writer.writerow(...list out data column headers...)
-#   writer.writerows(...variable for your zipped list...)
+# Taking results and writing them to a new csv file.
+output_path = os.path.join("output", "budget_analysis.csv")
+with open(output_path, 'w', newline="") as csvfile:
+    csvwriter = csv.writer(csvfile, delimiter=',')
+    csvwriter.writerow(["date", "profit_loss", "average_changes"])
+    csvwriter.writerows(z)
